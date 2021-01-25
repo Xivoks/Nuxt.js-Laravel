743931ca072dc03904b3b1781586ebccb26eb25d
Xivoks <xivoks1@gmail.com>
2021-01-25 11:12:38 +0100

    First commit

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,60 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'frontend',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'ant-design-vue/dist/antd.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/antd-ui'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+  ],
+  auth:{
+    strategies:{
+      local:{
+        endpoints:{
+          login:{url:'/login',method:'post'},
+          logout:{url:'/logout',method:'post'},
+          user:{url:'/api/user',method:'post'},
+        },
+        tokenRequired: false,
+        tokenType: false,
+      }
+    }
+  },
+  axios: {
+    baseURL:'http://localhost:8000/',
+    credentials:true
+  },
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
